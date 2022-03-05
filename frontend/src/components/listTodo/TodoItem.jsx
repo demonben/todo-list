8e476e1c209e50cf49e10859dd740a11010668e0
Dima <benensondimitrii@gmail.com>
2022-03-05 19:30:22 +0200

    added style for todoitem

--- frontend/src/components/listTodo/TodoItem.jsx
+++ frontend/src/components/listTodo/TodoItem.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-export default function TodoItem({ todoItem }) {
+export default function TodoItem({ todoItem, deleteHandler }) {
   return (
-    <div>
-      <li>{todoItem.text}</li>
+    <div className="todo">
+      <li className="todo-item">{todoItem.text}</li>
+      <button onClick={deleteHandler} className="complete-btn">
+        <i className="fas fa-check">Done</i>
+      </button>
+      <button className="trash-btn">
+        <i className="fas fa-trash">Delete</i>
+      </button>
     </div>
   );
 }
