56ed190fb2bf2a81b695ceef7126ce31ae987aed
Dima <benensondimitrii@gmail.com>
2022-03-06 14:52:26 +0200

    added logic for completed

--- frontend/src/components/listTodo/TodoItem.jsx
+++ frontend/src/components/listTodo/TodoItem.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
 export default function TodoItem({ todoItem, setTodoList, todoList }) {
-  const { text, id } = todoItem;
-
+  const { text, id, completed } = todoItem;
   const deleteHandler = () => {
     setTodoList(todoList.filter((item) => item.id !== id));
   };
 
-
+  const completeHandler = () => {
+    setTodoList(
+      todoList.map((item) => {
+        if (item.id === id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button  className="complete-btn">
+      <li className={`todo-item ${completed ? "completed" : ""}`}>{text}</li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check">Done</i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
